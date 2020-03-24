f1dc5f9bebdd65b208e8ff51057e1170582d929e
TranMinhKhuong <tranminhkhuongph08470@gmail.com>
2020-03-24 14:43:50 +0700

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,18 @@ import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import {ConvertGenderPipe} from 
 './convert-gender.pipe'
+import { HttpClientModule } from '@angular/common/http';
+import { HotelService } from './hotel.service';
 
 @NgModule({
   imports:      [ 
-    BrowserModule, FormsModule,
+    BrowserModule, FormsModule,HttpClientModule,
     RouterModule.forRoot([
       {path:'',component: HotelsComponent}
     ])
   ],
   declarations: [ AppComponent,ConvertGenderPipe, HotelsComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [HotelService]
 })
 export class AppModule { }
---
+++ src/app/hotel.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl = 'https://5e79b50717314d0016133423.mockapi.io/hotels'; 
+@Injectable()
+export class HotelService {
+
+  constructor() { }
+
+}
