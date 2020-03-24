63520ffcdc1227ecd53f5129e9ca9859e8cf6dac
TranMinhKhuong <tranminhkhuongph08470@gmail.com>
2020-03-24 14:37:59 +0700

    1 file modified, 2 added, 9 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
+
+import { HotelsComponent } from './hotels/hotels.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { HospitalListComponent } from './hospital-list/hospital-list.component';
-import { ConvertGenderPipe } from './convert-gender.pipe';
-import { HospitalDetailComponent } from './hospital-detail/hospital-detail.component';
-import { HospitalFormComponent } from './hospital-form/hospital-form.component';
-
+import {ConvertGenderPipe} from 
+'./convert-gender.pipe'
 
 @NgModule({
   imports:      [ 
     BrowserModule, FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HospitalListComponent},
-      {path: 'hospital-detail/:hospitalId', component: HospitalDetailComponent},
-      {path: 'add-hospital', component: HospitalFormComponent},
-      {path: 'edit-hospital/:hospitalId', component: HospitalFormComponent}
+      {path:'',component: HotelsComponent}
     ])
   ],
-  declarations: [ AppComponent, HospitalListComponent, ConvertGenderPipe, HospitalDetailComponent, HospitalFormComponent ],
+  declarations: [ AppComponent,ConvertGenderPipe, HotelsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
