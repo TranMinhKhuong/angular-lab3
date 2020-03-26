6a3ab239cf752f27000516d48d4e61a5718de14b
TranMinhKhuong <tranminhkhuongph08470@gmail.com>
2020-03-26 15:02:14 +0700

    2 files modified

--- src/app/hotel-form/hotel-form.component.ts
+++ src/app/hotel-form/hotel-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {HotelService} from '../hotel.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-hotel-form',
@@ -8,13 +9,15 @@ import {HotelService} from '../hotel.service';
   styleUrls: ['./hotel-form.component.css']
 })
 export class HotelFormComponent implements OnInit {
-  hotel={
-    id: null,
-    name: "",
-    logo: "",
-    address: "",
-    country: ""
-  }
+
+  // sử dựng Form group để đưa dữ liệu vào.
+  hotelForm= new FormGroup({
+    id: new FormControl(null),
+    name: new FormControl(''),
+    logo: new FormControl(''),
+    address: new FormControl(''),
+    country: new FormControl(''),
+  });
   // có 3 cái DI
   constructor(
     private hotelService: HotelService,
@@ -26,7 +29,7 @@ export class HotelFormComponent implements OnInit {
     this.activeRoute.paramMap.subscribe(params => {
       let hotelId = params.get('id');
       this.hotelService.getHotelById(hotelId).subscribe(data => {
-        this.hotel = data;
+        this.hotelForm.setValue(data);
       })
     })
   }
